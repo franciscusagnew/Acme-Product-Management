7c4699dc836db397dfe63313423aa674377cb03b
Franciscus Agnew <agnew_f@hotmail.com>
2017-03-30 03:11:53 -0700

    Added Event Handling with Event Bidding to Show/Hide Image button for product-list.component.html

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -10,6 +10,7 @@ export class ProductListComponent {
     imageWidth: number = 50;
     imageHeight: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
         "productId": 1,
@@ -42,4 +43,8 @@ export class ProductListComponent {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
